f923250088f5fe18bdb25f38483a6f151fc4aa3a
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-07-14 21:29:19 +0200

    Upstream source code, fix crashes in recent versions, made "deadlock-integrations" optional, add support for "LoaderRefux"

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,20 +3,16 @@ require("prototypes.shared")
 -- Add new tiers based on crating mod
 
 -- iterate through tiers to add entities
-for i = 4, DCM.TIERS do
-	local machine = table.deepcopy(data.raw["assembling-machine"]["deadlock-machine-packer-entity-1"])
-	machine.name = "deadlock-machine-packer-entity-" .. i
-	machine.icons[2].tint = DCM.TIER_COLOURS[i]
-	machine.minable.result = "deadlock-machine-packer-item-" .. i
-	machine.crafting_speed = i
-	--machine.module_specification = {
-	--	module_info_icon_shift = {0, 0.8},
-	--	module_slots = 1
-	--	}
-	--machine.allowed_effects = {"consumption"}
-	--machine.crafting_categories = {"packing"}
-	machine.max_health = DCM.get_scale(i, DCM.TIERS, 300, 400)
-	local energy_table = DCM.get_energy_table(i, DCM.TIERS, 500, 1000, 0.1, 5 - i)
+for tier = 4, DCM.TIERS do
+	local machine = table.deepcopy(data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "1"])
+	machine.name = DCM.MACHINE_PREFIX .. tier
+	machine.icons[2].tint = DCM.TIER_COLOURS[tier]
+
+	machine.minable.result = DCM.MACHINE_PREFIX .. tier
+	machine.crafting_speed = tier
+	machine.module_specification.module_slots = tier - 2
+	machine.max_health = DCM.get_scale(tier, DCM.TIERS, 300, 400)
+	local energy_table = DCM.get_energy_table(tier, DCM.TIERS, 500, 1000, 0.1, 5 - tier)
 	machine.energy_usage = energy_table.active
 	machine.energy_source = {
 		type = "electric",
@@ -24,245 +20,127 @@ for i = 4, DCM.TIERS do
 		drain = energy_table.passive,
 		emissions_per_second_per_watt = energy_table.emissions -- replaces old emissions parameter
 	}
-	--machine.working_sound = {filename = "__DeadlockCrating__/sounds/deadlock-crate-machine.ogg", volume = 0.7}
-	machine.animation.layers[3].hr_version.tint = DCM.TIER_COLOURS[i]
-	machine.animation.layers[3].tint = DCM.TIER_COLOURS[i]
+	machine.animation.layers[3].hr_version.tint = DCM.TIER_COLOURS[tier]
+	machine.animation.layers[3].tint = DCM.TIER_COLOURS[tier]
+	machine.animation.layers[2].hr_version.tint = DCM.TIER_COLOURS[tier]
+	machine.animation.layers[2].tint = DCM.TIER_COLOURS[tier]
 
-	machine.working_visualisations[1].animation.hr_version.tint = DCM.brighter_colour(DCM.TIER_COLOURS[i])
-	machine.working_visualisations[1].animation.tint = DCM.brighter_colour(DCM.TIER_COLOURS[i])
-	machine.working_visualisations[1].light.color = DCM.brighter_colour(DCM.TIER_COLOURS[i])
-	--[[ 	machine.animation = {
-		layers = {
-			{
-				hr_version = {
-					draw_as_shadow = true,
-					filename = "__DeadlockCrating__/graphics/entities/high/crating-shadow.png",
-					animation_speed = 1 / i,
-					repeat_count = 60,
-					height = 192,
-					scale = 0.5,
-					shift = {1.5, 0},
-					width = 384
-				},
-				draw_as_shadow = true,
-				filename = "__DeadlockCrating__/graphics/entities/low/crating-shadow.png",
-				animation_speed = 1 / i,
-				repeat_count = 60,
-				height = 96,
-				scale = 1,
-				shift = {1.5, 0},
-				width = 192
-			},
-			{
-				hr_version = {
-					filename = "__DeadlockCrating__/graphics/entities/high/crating-base.png",
-					animation_speed = 1 / i,
-					priority = "high",
-					frame_count = 60,
-					line_length = 10,
-					height = 192,
-					scale = 0.5,
-					shift = {0, 0},
-					width = 192
-				},
-				filename = "__DeadlockCrating__/graphics/entities/low/crating-base.png",
-				animation_speed = 1 / i,
-				priority = "high",
-				frame_count = 60,
-				line_length = 10,
-				height = 96,
-				scale = 1,
-				shift = {0, 0},
-				width = 96
-			},
-			{
-				hr_version = {
-					filename = "__DeadlockCrating__/graphics/entities/high/crating-mask.png",
-					animation_speed = 1 / i,
-					priority = "high",
-					repeat_count = 60,
-					height = 192,
-					scale = 0.5,
-					shift = {0, 0},
-					width = 192,
-					tint = DCM.TIER_COLOURS[i]
-				},
-				filename = "__DeadlockCrating__/graphics/entities/low/crating-mask.png",
-				animation_speed = 1 / i,
-				priority = "high",
-				repeat_count = 60,
-				height = 96,
-				scale = 1,
-				shift = {0, 0},
-				width = 96,
-				tint = DCM.TIER_COLOURS[i]
-			}
-		}
-	} ]]
-	--[[
-	machine.working_visualisations = {
-		{
-			animation = {
-				hr_version = {
-					animation_speed = 1 / i,
-					blend_mode = "additive",
-					filename = "__DeadlockCrating__/graphics/entities/high/crating-working.png",
-					frame_count = 30,
-					line_length = 10,
-					height = 192,
-					priority = "high",
-					scale = 0.5,
-					tint = DCM.brighter_colour(DCM.TIER_COLOURS[i]),
-					width = 192
-				},
-				animation_speed = 1 / i,
-				blend_mode = "additive",
-				filename = "__DeadlockCrating__/graphics/entities/low/crating-working.png",
-				frame_count = 30,
-				line_length = 10,
-				height = 96,
-				priority = "high",
-				tint = DCM.brighter_colour(DCM.TIER_COLOURS[i]),
-				width = 96
-			},
-			light = {
-				color = DCM.brighter_colour(DCM.TIER_COLOURS[i]),
-				intensity = 0.4,
-				size = 9,
-				shift = {0, 0}
-			}
-		}
-	} ]]
+	machine.working_visualisations[1].animation.hr_version.tint = DCM.brighter_colour(DCM.TIER_COLOURS[tier])
+	machine.working_visualisations[1].animation.tint = DCM.brighter_colour(DCM.TIER_COLOURS[tier])
+	machine.working_visualisations[1].light.color = DCM.brighter_colour(DCM.TIER_COLOURS[tier])
 	data:extend {machine}
 end
 
 -- crating machine items
-for i = 4, DCM.TIERS do
+for tier = 4, DCM.TIERS do
 	data:extend {
 		{
 			type = "item",
-			name = "deadlock-machine-packer-item-" .. i,
+			name = DCM.MACHINE_PREFIX .. tier,
 			subgroup = "production-machine",
 			stack_size = 50,
 			icons = {
-				{icon = "__DeadlockCrating__/graphics/icons/crating-icon-base-" .. DCM.ITEM_ICON_SIZE .. ".png"},
-				{icon = "__DeadlockCrating__/graphics/icons/crating-icon-mask-" .. DCM.ITEM_ICON_SIZE .. ".png", tint = DCM.TIER_COLOURS[i]}
+				{
+					icon = "__DeadlockCrating__/graphics/icons/mipmaps/crating-icon-base.png",
+					icon_size = DCM.ITEM_ICON_SIZE,
+					icon_mipmaps = 4
+				},
+				{
+					icon = "__DeadlockCrating__/graphics/icons/mipmaps/crating-icon-mask.png",
+					icon_size = DCM.ITEM_ICON_SIZE,
+					tint = DCM.TIER_COLOURS[tier],
+					icon_mipmaps = 4
+				}
 			},
 			icon_size = DCM.ITEM_ICON_SIZE,
-			order = "z" .. i,
-			place_result = "deadlock-machine-packer-entity-" .. i,
+			order = "z" .. tier,
+			place_result = DCM.MACHINE_PREFIX .. tier,
 			flags = {}
 		}
 	}
 end
 
-if data.raw.item["ultimate-transport-belt-beltbox"] then
-	-- RECIPES for the machines
-	data:extend {
-		{
-			type = "recipe",
-			name = "deadlock-machine-packer-recipe-4",
-			enabled = false,
-			ingredients = {
-				{"deadlock-machine-packer-item-3", 1},
-				{"turbo-transport-belt-beltbox", 4},
-				{"steel-plate", 10}
-			},
-			result = "deadlock-machine-packer-item-4",
-			energy_required = 9.0
+-- RECIPES for the machines
+data:extend {
+	{
+		type = "recipe",
+		name = DCM.MACHINE_PREFIX .. "4",
+		enabled = false,
+		ingredients = {
+			{DCM.MACHINE_PREFIX .. "3", 1},
+			{DCM.LOADER_FOUR, 4},
+			{"steel-plate", 10}
 		},
-		{
-			type = "recipe",
-			name = "deadlock-machine-packer-recipe-5",
-			enabled = false,
-			ingredients = {
-				{"deadlock-machine-packer-item-4", 1},
-				{"ultimate-transport-belt-beltbox", 4},
-				{"steel-plate", 10}
-			},
-			result = "deadlock-machine-packer-item-5",
-			energy_required = 10.0
-		}
-	}
-else
-	data:extend {
-		{
-			type = "recipe",
-			name = "deadlock-machine-packer-recipe-4",
-			enabled = false,
-			ingredients = {
-				{"deadlock-machine-packer-item-3", 2},
-				{"express-transport-belt-beltbox", 4},
-				{"steel-plate", 10}
-			},
-			result = "deadlock-machine-packer-item-4",
-			energy_required = 9.0
+		result = DCM.MACHINE_PREFIX .. "4",
+		energy_required = 9.0
+	},
+	{
+		type = "recipe",
+		name = DCM.MACHINE_PREFIX .. "5",
+		enabled = false,
+		ingredients = {
+			{DCM.MACHINE_PREFIX .. "4", 1},
+			{DCM.LOADER_FIVE, 4},
+			{"steel-plate", 10}
 		},
-		{
-			type = "recipe",
-			name = "deadlock-machine-packer-recipe-5",
-			enabled = false,
-			ingredients = {
-				{"deadlock-machine-packer-item-4", 2},
-				{"express-transport-belt-beltbox", 4},
-				{"steel-plate", 20}
-			},
-			result = "deadlock-machine-packer-item-5",
-			energy_required = 10.0
-		}
+		result = DCM.MACHINE_PREFIX .. "5",
+		energy_required = 10.0
 	}
-end
+}
 
 -- here we adjust the previous tiers
 
-if data.raw.recipe["deadlock-machine-packer-recipe-1"] then
+if data.raw.recipe[DCM.MACHINE_PREFIX .. "1"] then
 	local t1_crater_ingredients = {
-		{"transport-belt-beltbox", 8},
+		{DCM.LOADER_ONE, 8},
 		{"steel-plate", 10}
 	}
-	data.raw.recipe["deadlock-machine-packer-recipe-1"].ingredients = t1_crater_ingredients
+	data.raw.recipe[DCM.MACHINE_PREFIX .. "1"].ingredients = t1_crater_ingredients
 end
-if data.raw.recipe["deadlock-machine-packer-recipe-2"] then
+if data.raw.recipe[DCM.MACHINE_PREFIX .. "2"] then
 	local t2_crater_ingredients = {
-		{"deadlock-machine-packer-item-1", 1},
-		{"fast-transport-belt-beltbox", 4},
+		{DCM.MACHINE_PREFIX .. "1", 1},
+		{DCM.LOADER_TWO, 4},
 		{"steel-plate", 10}
 	}
-	data.raw.recipe["deadlock-machine-packer-recipe-2"].ingredients = t2_crater_ingredients
+	data.raw.recipe[DCM.MACHINE_PREFIX .. "2"].ingredients = t2_crater_ingredients
 end
-if data.raw.recipe["deadlock-machine-packer-recipe-3"] then
+if data.raw.recipe[DCM.MACHINE_PREFIX .. "3"] then
 	local t3_crater_ingredients = {
-		{"deadlock-machine-packer-item-2", 1},
-		{"express-transport-belt-beltbox", 4},
+		{DCM.MACHINE_PREFIX .. "2", 1},
+		{DCM.LOADER_THREE, 4},
 		{"steel-plate", 10}
 	}
-	data.raw.recipe["deadlock-machine-packer-recipe-3"].ingredients = t3_crater_ingredients
+	data.raw.recipe[DCM.MACHINE_PREFIX .. "3"].ingredients = t3_crater_ingredients
 end
 
 function getCraftingSpeed(level)
 	return level * settings.startup["bobmods-logistics-beltspeedperlevel"].value / 15
 end
 
-if settings.startup["bobmods-logistics-beltoverhaulspeed"] and settings.startup["bobmods-logistics-beltoverhaulspeed"].value then
+if
+	settings.startup["bobmods-logistics-beltoverhaulspeed"] and
+		settings.startup["bobmods-logistics-beltoverhaulspeed"].value
+ then
 	-- yellow
-	if data.raw["assembling-machine"]["deadlock-machine-packer-entity-1"] then
-		data.raw["assembling-machine"]["deadlock-machine-packer-entity-1"].crafting_speed = getCraftingSpeed(2)
+	if data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "1"] then
+		data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "1"].crafting_speed = getCraftingSpeed(2)
 	end
 	-- red
-	if data.raw["assembling-machine"]["deadlock-machine-packer-entity-2"] then
-		data.raw["assembling-machine"]["deadlock-machine-packer-entity-2"].crafting_speed = getCraftingSpeed(3)
+	if data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "2"] then
+		data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "2"].crafting_speed = getCraftingSpeed(3)
 	end
 	-- blue
-	if data.raw["assembling-machine"]["deadlock-machine-packer-entity-3"] then
-		data.raw["assembling-machine"]["deadlock-machine-packer-entity-3"].crafting_speed = getCraftingSpeed(4)
+	if data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "3"] then
+		data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "3"].crafting_speed = getCraftingSpeed(4)
 	end
 	-- purple
-	if data.raw["assembling-machine"]["deadlock-machine-packer-entity-4"] then
-		data.raw["assembling-machine"]["deadlock-machine-packer-entity-4"].crafting_speed = getCraftingSpeed(5)
+	if data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "4"] then
+		data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "4"].crafting_speed = getCraftingSpeed(5)
 	end
 	-- green
-	if data.raw["assembling-machine"]["deadlock-machine-packer-entity-5"] then
-		data.raw["assembling-machine"]["deadlock-machine-packer-entity-5"].crafting_speed = getCraftingSpeed(6)
+	if data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "5"] then
+		data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. "5"].crafting_speed = getCraftingSpeed(6)
 	end
 end
 data:extend(
@@ -276,35 +154,35 @@ data:extend(
 	}
 )
 
-for i = 1, DCM.TIERS do
-	if data.raw["assembling-machine"]["deadlock-machine-packer-entity-" .. i] then
-		data.raw["assembling-machine"]["deadlock-machine-packer-entity-" .. i].fast_replaceable_group = "crating-machine"
-		if (i < 5) then
-			data.raw["assembling-machine"]["deadlock-machine-packer-entity-" .. i].next_upgrade = "deadlock-machine-packer-entity-" .. i + 1
+for tier = 1, DCM.TIERS do
+	if data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. tier] then
+		data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. tier].fast_replaceable_group = "crating-machine"
+		if (tier < 5) then
+			data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. tier].next_upgrade = DCM.MACHINE_PREFIX .. tier + 1
 		else
-			data.raw["assembling-machine"]["deadlock-machine-packer-entity-" .. i].next_upgrade = ""
+			data.raw["assembling-machine"][DCM.MACHINE_PREFIX .. tier].next_upgrade = ""
 		end
-		data.raw.item["deadlock-machine-packer-item-" .. i].subgroup = "production-machine-packing"
+		data.raw.item[DCM.MACHINE_PREFIX .. tier].subgroup = "production-machine-packing"
 	end
 end
 
 -- research
-for i = 4, 5 do
+for tier = 4, 5 do
 	local recipeeffects = {
 		[1] = {
 			type = "unlock-recipe",
-			recipe = "deadlock-machine-packer-recipe-" .. i
+			recipe = DCM.MACHINE_PREFIX .. tier
 		}
 	}
 	local research = table.deepcopy(data.raw.technology[DCM.TECH_PREFIX .. 3])
 	research.effects = recipeeffects
 	research.icons = {
-		{icon = "__DeadlockCrating__/graphics/icons/crating-icon-base-128.png"},
-		{icon = "__DeadlockCrating__/graphics/icons/crating-icon-mask-128.png", tint = DCM.TIER_COLOURS[i]}
+		{icon = "__DeadlockCrating__/graphics/icons/square/crating-icon-base-128.png"},
+		{icon = "__DeadlockCrating__/graphics/icons/square/crating-icon-mask-128.png", tint = DCM.TIER_COLOURS[tier]}
 	}
-	research.name = DCM.TECH_PREFIX .. i
+	research.name = DCM.TECH_PREFIX .. tier
 	research.unit.count = research.unit.count * 2
-	research.prerequisites = {DCM.TECH_PREFIX .. i - 1}
+	research.prerequisites = {DCM.TECH_PREFIX .. tier - 1}
 	research.upgrade = false
 	data:extend({research})
 end
--- prototypes/shared.lua
+++ prototypes/shared.lua
@@ -1,9 +1,6 @@
 -- Here we need to add the tiers
 DCM = {}
 
-----------------------------
--- copy from original mod, change:
-
 -- size of machine icons and crate background
 DCM.ITEM_ICON_SIZE = 32
 
@@ -17,22 +14,49 @@ DCM.TAB_TIERS = DCM.TIERS + 1
 
 -- which items can be crated, in which tier
 DCM.ITEM_TIER = {
-	[4] = { "wood", "iron-ore", "copper-ore", "stone", "coal", "iron-plate", "copper-plate", "steel-plate", "stone-brick" },
-	[5] = { "copper-cable", "iron-gear-wheel", "iron-stick", "sulfur", "plastic-bar", "solid-fuel", "electronic-circuit", "advanced-circuit" },
+	[4] = {"wood", "iron-ore", "copper-ore", "stone", "coal", "iron-plate", "copper-plate", "steel-plate", "stone-brick"},
+	[5] = {
+		"copper-cable",
+		"iron-gear-wheel",
+		"iron-stick",
+		"sulfur",
+		"plastic-bar",
+		"solid-fuel",
+		"electronic-circuit",
+		"advanced-circuit"
+	}
 }
 
 DCM.TIER_COLOURS = {
-	[1] = {r=210, g=180, b=80},
-	[2] = {r=210, g=60, b=60},
-    [3] = {r=80, g=180, b=210},
-    [4] =  {r=165,g=10,b=225},
-    [5] =  {r=10,g=225,b=25},
+	[1] = {r = 210, g = 180, b = 80},
+	[2] = {r = 210, g = 60, b = 60},
+	[3] = {r = 80, g = 180, b = 210},
+	[4] = {r = 165, g = 10, b = 225},
+	[5] = {r = 10, g = 225, b = 25}
 }
 
+-- which loaders to use based on bobs logistic
+if mods["boblogistics"] and mods["LoaderRedux"] then
+	DCM.LOADER_ONE = "loader"
+	DCM.LOADER_TWO = "fast-loader"
+	DCM.LOADER_THREE = "express-loader"
+	DCM.LOADER_FOUR = "purple-loader"
+	DCM.LOADER_FIVE = "green-loader"
+else
+	DCM.LOADER_ONE = "transport-belt-beltbox"
+	DCM.LOADER_TWO = "fast-transport-belt-beltbox"
+	DCM.LOADER_THREE = "express-transport-beltbox"
+	if data.raw.item["ultimate-transport-belt-beltbox"] then
+		DCM.LOADER_FOUR = "express-transport-belt"
+		DCM.LOADER_FIVE = "express-transport-belt"
+	else
+		DCM.LOADER_FOUR = "ultimate-transport-belt-beltbox"
+		DCM.LOADER_FIVE = "turbo-transport-belt-beltbox"
+	end
+end
 
+DCM.MACHINE_PREFIX = "deadlock-crating-machine-"
 DCM.TECH_PREFIX = "deadlock-crating-"
-
-
 
 -- for calculating scales of energy, health etc.
 function DCM.get_scale(this_tier, tiers, lowest, highest)
@@ -47,12 +71,12 @@ function DCM.get_energy_table(this_tier, tiers, lowest, highest, passive_multipl
 	return {
 		passive = passive_energy_usage .. "KW", -- passive energy drain as a string
 		active = active_energy_usage .. "KW", -- active energy usage as a string
-		emissions = pollution / active_energy_usage / 1000, -- pollution/s/W
+		emissions = pollution / active_energy_usage / 1000 -- pollution/s/W
 	}
 end
 
 -- brighter version of tier colour for working vis glow & lights
 function DCM.brighter_colour(c)
 	local w = 240
-	return { r = math.floor((c.r + w)/2), g = math.floor((c.g + w)/2), b = math.floor((c.b + w)/2) }
+	return {r = math.floor((c.r + w) / 2), g = math.floor((c.g + w) / 2), b = math.floor((c.b + w) / 2)}
 end
